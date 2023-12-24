2d1ef97fd3cce2a580cb34986b60ef66b18afd52
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2023-12-24 16:39:02 +0330

    add rate to response

--- api/detect_suicide_api.py
+++ api/detect_suicide_api.py
@@ -22,6 +22,9 @@ class InputData(BaseModel):
 
 @app.post("/suicide-detection")
 async def detect_suicide(input_data: InputData):
-    result, english_text = inference.predict_suicidal_text_loaded(input_data.text)
-    return {"result": result, "english_text": english_text}
+    result, english_text, score = inference.predict_suicidal_text_loaded(input_data.text)
+    non_suicidal_rate = round(score[0]*100, 0)
+    suicidal_rate = round(score[1] * 100, 0)
+    return {"result": result, "english_text": english_text, "suicidal_rate": suicidal_rate,
+            "non_suicidal_rate": non_suicidal_rate}
 
--- inference.py
+++ inference.py
@@ -3,7 +3,10 @@ from lib import translation
 
 # Simple model
 # Load the saved classifier and vectorizer
+# API
 model_filename = '../model.joblib'
+# Local
+# model_filename = 'model.joblib'
 loaded_classifier, loaded_tfidf_vectorizer = joblib.load(model_filename)
 
 
@@ -11,13 +14,14 @@ loaded_classifier, loaded_tfidf_vectorizer = joblib.load(model_filename)
 def predict_suicidal_text_loaded(text):
     english_text = translation.translate_persian_to_english(text)
     text_tfidf = loaded_tfidf_vectorizer.transform([english_text])
+    score = loaded_classifier.predict_proba(text_tfidf)[0]
     prediction = loaded_classifier.predict(text_tfidf)
-    return prediction[0], english_text
+    return prediction[0], english_text, score
 
 
 # Example usage with the loaded model
-# user_text = "احساس خوبی به زندگی ندارم و نمیخوام که ادامه بدم"
-# english_text = tranlation.translate_persian_to_english(user_text)
-# print("User Input:", english_text)
-# loaded_prediction = predict_suicidal_text_loaded(english_text)
+# user_text = "فوتبال قشنگ است"
+# # english_text = translation.translate_persian_to_english(user_text)
+# # print("User Input:", english_text)
+# loaded_prediction = predict_suicidal_text_loaded(user_text)
 # print(f'Text classification: {loaded_prediction}')
