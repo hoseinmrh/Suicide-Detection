dba85589fcf3858c36765b281b68f41d541516a6
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2023-12-24 13:01:40 +0330

    minor changes

--- inference.py
+++ inference.py
@@ -6,11 +6,13 @@ from text_processing import create_tfidf_vectorizer
 model_filename = 'model.joblib'
 loaded_classifier, loaded_tfidf_vectorizer = joblib.load(model_filename)
 
+
 # Function to predict suicidal text using the loaded model
 def predict_suicidal_text_loaded(text):
     text_tfidf = loaded_tfidf_vectorizer.transform([text])
     prediction = loaded_classifier.predict(text_tfidf)
     return prediction[0]
+
 
 # Example usage with the loaded model
 user_text = "I fill so empty without her and I can't live that way"
