09fb4bf73dfdbcafd7a1a18ed0a36ab78d1f1b75
Yasin Rezaei <yasinrezaei@hotmail.com>
2023-12-25 17:20:49 +0330

    db models added

--- django/suicide_detection/main/models.py
+++ django/suicide_detection/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class SuicideTest(models.Model):
+    text = models.TextField(verbose_name='text')
+    time = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.text
+    
+class TestResult(models.Model):
+    test = models.ForeignKey(SuicideTest,on_delete = models.CASCADE,verbose_name='Suicide Test')
+    suicidal_rate = models.IntegerField(verbose_name = 'Suicidal Rate')
+    non_suicidal_rate = models.IntegerField(verbose_name = 'Non Suicidal Rate')
+
+    
