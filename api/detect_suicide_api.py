70b0d241405d8303e7c1ec43ce22ff99bcdb1b66
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2023-12-26 16:23:47 +0330

    changes in fast api

--- api/detect_suicide_api.py
+++ api/detect_suicide_api.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sys
-from enum import Enum
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 import inference
@@ -9,29 +8,21 @@ import inference
 app = FastAPI()
 
 
-class Languages(Enum):
-    English = "en"
-    Farsi = "fa"
-
-
 class InputData(BaseModel):
     text: str
-    lang: Languages
 
     # Sample input
     class Config:
         json_schema_extra = {
             "example": {
-                "text": "متن خود را وارد کنید",
-                "lang": "fa"
+                "text": "متن خود را وارد کنید"
             }
         }
 
 
 @app.post("/suicide-detection")
 async def detect_suicide(input_data: InputData):
-    result, english_text, score = inference.predict_suicidal_text_loaded(input_data.text,
-                                                                         input_data.lang.value)
+    result, english_text, score = inference.predict_suicidal_text_loaded(input_data.text)
     non_suicidal_rate = round(score[0]*100, 0)
     suicidal_rate = round(score[1] * 100, 0)
     return {"result": result, "english_text": english_text, "suicidal_rate": suicidal_rate,
