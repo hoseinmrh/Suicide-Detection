7439f5bf4e162c3a01a0a4411d80270a084ba3b7
Hosein Mirhoseini <hoseinmirhoseini64@gmail.com>
2024-01-17 11:41:09 +0330

    minor changes

--- inference.py
+++ inference.py
@@ -12,20 +12,18 @@ loaded_classifier, loaded_tfidf_vectorizer = joblib.load(model_filename)
 
 # Function to predict suicidal text using the loaded model
 def predict_suicidal_text_loaded(text):
+    # Detect the language of user text
     lang = lang_detection.language_detection(text)
     if lang != "en":
+        # If it's not English, translate it to English automatically
         text = translation.translate_to_english(text, lang)
     text_tfidf = loaded_tfidf_vectorizer.transform([text])
     score = loaded_classifier.predict_proba(text_tfidf)[0]
     prediction = loaded_classifier.predict(text_tfidf)
+    # Return prediction, Translated text to English and Scores
     return prediction[0], text, score
 
 
 # Example usage with the loaded model
-# user_text = "فوتبال قشنگ است"
-# # english_text = translation.translate_persian_to_english(user_text)
-# # print("User Input:", english_text)
-# loaded_prediction = predict_suicidal_text_loaded(user_text)
-# print(f'Text classification: {loaded_prediction}')
-
-# print(predict_suicidal_text_loaded("Secondo me, niente può migliorare e non c'è modo per me"))
+# Input in Italian
+# print(predict_suicidal_text_loaded("Non ho alcuna speranza e penso che non ci sia niente per me"))
